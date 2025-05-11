d2b10c4374e797e67a925c423520445e66e7d46f
JuanpinoTKW <juanpinito@gmail.com>
2025-05-11 15:59:04 +0000

    ADD: Añadiendo script

---
+++ script_dir.sh
@@ -0,0 +1,18 @@
+mkdir materia1
+mkdir materia2
+cat <<EOF>> script_dir.sh
+mkdir materia1
+mkdir materia2
+mkdir materia1/uni1
+cat <<EOF>> script_dir.sh
+mkdir materia1
+mkdir materia2
+cat <<EOF>> script_dir.sh
+mkdir materia1
+mkdir materia2
+mkdir materia1/uni1
+mkdir materia1/uni2
+mkdir materia1/uni3
+mkdir materia2/uni1
+mkdir materia2/uni2
+mkdir materia2/uni3
