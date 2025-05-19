74ff44072881e0695459483208de27c6685b2097
JuanpinoTKW <juanpinito@gmail.com>
2025-05-19 21:10:34 +0000

    ADD: Añadiendo un nuevo script

--- script_dir.sh
+++ script_dir.sh
@@ -1,18 +1,3 @@
-mkdir materia1
-mkdir materia2
-cat <<EOF>> script_dir.sh
-mkdir materia1
-mkdir materia2
-mkdir materia1/uni1
-cat <<EOF>> script_dir.sh
-mkdir materia1
-mkdir materia2
-cat <<EOF>> script_dir.sh
-mkdir materia1
-mkdir materia2
-mkdir materia1/uni1
-mkdir materia1/uni2
-mkdir materia1/uni3
-mkdir materia2/uni1
-mkdir materia2/uni2
-mkdir materia2/uni3
+mkdir -p {materia1,materia2}/{uni1,uni2,uni3}
+chmod u+x script_dir.sh
+
